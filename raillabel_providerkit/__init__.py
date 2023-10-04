25aca047fcff77b565ba28fdd816996cc15bf9e3
Tobias Klockau <tobias.klockau@deutschebahn.com>
2023-10-04 15:55:51 +0200

    feat: define package structure

---
+++ raillabel_providerkit/__init__.py
@@ -0,0 +1,10 @@
+# Copyright DB Netz AG and contributors
+# SPDX-License-Identifier: Apache-2.0
+"""A library for annotation providers of raillabel-formatted data."""
+from importlib import metadata
+
+try:
+    __version__ = metadata.version("raillabel-providerkit")
+except metadata.PackageNotFoundError:
+    __version__ = "0.0.0+unknown"
+del metadata
