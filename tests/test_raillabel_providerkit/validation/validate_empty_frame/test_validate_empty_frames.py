8945f532d6b8ff110b1f054a4e032fca19046581
Tobias Klockau <tobias.klockau@deutschebahn.com>
2024-10-21 13:35:41 +0200

    extract empty annotation creation

--- tests/test_raillabel_providerkit/validation/validate_empty_frame/test_validate_empty_frames.py
+++ tests/test_raillabel_providerkit/validation/validate_empty_frame/test_validate_empty_frames.py
@@ -8,23 +8,16 @@ from raillabel_providerkit.validation.validate_empty_frames.validate_empty_frame
     _is_frame_empty,
 )
 
-# == Tests ============================
 
 def test_is_frame_empty__true():
     frame = raillabel.format.Frame(uid=0, annotations={})
     assert _is_frame_empty(frame)
 
-def test_is_frame_empty__false():
+def test_is_frame_empty__false(empty_annotation):
     frame = raillabel.format.Frame(
-        uid=0, annotations={
-            "581b0df1-c4cf-4a97-828e-13dd740defe5": raillabel.format.Bbox(
-                uid=None,
-                object=None,
-                sensor=None,
-                attributes=None,
-                pos=raillabel.format.Point2d(0, 0),
-                size=raillabel.format.Size2d(0, 0),
-            )
+        uid=0,
+        annotations={
+            "581b0df1-c4cf-4a97-828e-13dd740defe5": empty_annotation
         }
     )
     assert not _is_frame_empty(frame)
