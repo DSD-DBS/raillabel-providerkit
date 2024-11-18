bed431895c1dfa5a3f995bed5b8ee6db7ca5b0ce
Tobias Klockau <tobias.klockau@deutschebahn.com>
2024-11-18 13:51:18 +0100

    chore: change company name

--- raillabel_providerkit/validation/__init__.py
+++ raillabel_providerkit/validation/__init__.py
@@ -1,4 +1,4 @@
-# Copyright DB Netz AG and contributors
+# Copyright DB InfraGO AG and contributors
 # SPDX-License-Identifier: Apache-2.0
 """Package for validating raillabel data regarding the format requirements."""
 
--- tests/test_raillabel_providerkit/validation/conftest.py
+++ tests/test_raillabel_providerkit/validation/conftest.py
@@ -1,4 +1,4 @@
-# Copyright DB Netz AG and contributors
+# Copyright DB InfraGO AG and contributors
 # SPDX-License-Identifier: Apache-2.0
 
 from validate_onthology.test_validate_onthology import (
