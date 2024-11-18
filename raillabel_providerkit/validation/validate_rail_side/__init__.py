1342aae28fab8279a7ae62a6e2d892045ae727cc
Niklas Freund <niklas.freund@deutschebahn.com>
2024-11-18 11:22:18 +0100

    docs: Correct validate_rail_side package description

--- raillabel_providerkit/validation/validate_rail_side/__init__.py
+++ raillabel_providerkit/validation/validate_rail_side/__init__.py
@@ -1,3 +1,3 @@
 # Copyright DB Netz AG and contributors
 # SPDX-License-Identifier: Apache-2.0
-"""Package for validating a scene for empty frames."""
+"""Package for validating a scene for rail side errors."""
