9f1394f077f72ac3c8418811aa3f80380aa2c9ff
Niklas Freund <niklas.freund@deutschebahn.com>
2025-01-10 10:25:50 +0100

    feat: implement _multi_reference_attribute

---
+++ raillabel_providerkit/validation/validate_onthology/_onthology_classes/_attributes/_multi_reference_attribute.py
@@ -0,0 +1,68 @@
+# Copyright DB InfraGO AG and contributors
+# SPDX-License-Identifier: Apache-2.0
+
+from __future__ import annotations
+
+import typing as t
+from dataclasses import dataclass
+from uuid import UUID
+
+from raillabel_providerkit.validation import Issue, IssueIdentifiers, IssueType
+from raillabel_providerkit.validation.validate_onthology._onthology_classes._scope import (
+    _Scope,
+)
+
+from ._attribute_abc import _Attribute
+
+
+@dataclass
+class _MultiReferenceAttribute(_Attribute):
+    @classmethod
+    def supports(cls, data: dict) -> bool:
+        return "attribute_type" in data and data["attribute_type"] == "multi-reference"
+
+    @classmethod
+    def fromdict(cls, data: dict) -> _MultiReferenceAttribute:
+        if not cls.supports(data):
+            raise ValueError
+
+        return _MultiReferenceAttribute(
+            optional=data.get("optional", False),
+            scope=_Scope(data["scope"]),
+            sensor_types=data.get("sensor_types", ["camera", "lidar", "radar"]),
+        )
+
+    def check_type_and_value(
+        self,
+        attribute_name: str,
+        attribute_values: bool | float | str | list,
+        identifiers: IssueIdentifiers,
+    ) -> list[Issue]:
+        if type(attribute_values) is not list:
+            return [
+                Issue(
+                    type=IssueType.ATTRIBUTE_TYPE,
+                    reason=(
+                        f"Attribute '{attribute_name}' is of type"
+                        f" {attribute_values.__class__.__name__} (should be 'list')."
+                    ),
+                    identifiers=identifiers,
+                )
+            ]
+
+        attribute_value: t.Any
+        try:
+            for attribute_value in attribute_values:
+                UUID(attribute_value)
+        except (ValueError, AttributeError):
+            return [
+                Issue(
+                    type=IssueType.ATTRIBUTE_VALUE,
+                    reason=(
+                        f"Attribute '{attribute_name}' has a non-UUID value '{attribute_value}'."
+                    ),
+                    identifiers=identifiers,
+                )
+            ]
+
+        return []
