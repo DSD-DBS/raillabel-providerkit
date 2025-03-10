df8fcbe413de1925354a9d0452336f6266a67eaa
unexcellent <>
2025-03-10 15:21:49 +0100

    test: refactor validate integration tests

--- tests/validation/test_validate.py
+++ tests/validation/test_validate.py
@@ -23,8 +23,7 @@ def scene_to_dict(scene: Scene) -> dict:
 
 def test_no_issues_in_empty_scene_dict():
     scene_dict = {"openlabel": {"metadata": {"schema_version": "1.0.0"}}}
-    actual = validate(scene_dict)
-    assert len(actual) == 0
+    assert len(validate(scene_dict)) == 0
 
 
 def test_no_issues_in_empty_scene_path(tmp_path):
@@ -32,24 +31,23 @@ def test_no_issues_in_empty_scene_path(tmp_path):
     scene_path = tmp_path / "empty_scene.json"
     write_to_json(scene_dict, scene_path)
 
-    actual = validate(scene_path)
-    assert len(actual) == 0
+    assert len(validate(scene_path)) == 0
 
 
-def test_schema_issues():
+def test_validate_schema_included():
     scene_dict = {"openlabel": {}}
-    actual = validate(scene_dict)
-    assert len(actual) == 1
+    assert len(validate(scene_dict)) == 1
 
 
-def test_empty_frame_issues():
-    scene_dict = json.loads(SceneBuilder.empty().add_frame().result.to_json().model_dump_json())
+def test_validate_empty_frames_included():
+    scene = SceneBuilder.empty().add_frame().result
+    scene_dict = scene_to_dict(scene)
 
-    actual = validate(scene_dict)
-    assert len(actual) == 1
+    assert len(validate(scene_dict, validate_for_empty_frames=False)) == 0
+    assert len(validate(scene_dict, validate_for_empty_frames=True)) == 1
 
 
-def test_rail_side_issues():
+def test_validate_rail_side_included():
     scene = (
         SceneBuilder.empty()
         .add_poly2d(
@@ -57,7 +55,7 @@ def test_rail_side_issues():
                 Point2d(0, 0),
                 Point2d(0, 1),
             ],
-            attributes={"railSide": "rightRail"},
+            attributes={"railSide": "rightRail", "trackID": 0},
             object_name="track_0001",
             sensor_id="rgb_center",
         )
@@ -66,47 +64,50 @@ def test_rail_side_issues():
                 Point2d(1, 0),
                 Point2d(1, 1),
             ],
-            attributes={"railSide": "leftRail"},
+            attributes={"railSide": "leftRail", "trackID": 0},
             object_name="track_0001",
             sensor_id="rgb_center",
         )
         .result
     )
-    scene_dict = json.loads(scene.to_json().model_dump_json())
+    scene_dict = scene_to_dict(scene)
 
-    actual = validate(scene_dict, validate_for_missing_ego_track=False)
-    assert len(actual) == 1
+    assert len(validate(scene_dict, validate_for_rail_side_order=False)) == 0
+    assert len(validate(scene_dict, validate_for_rail_side_order=True)) == 1
 
 
-def test_missing_ego_track_issue():
-    scene_dict = json.loads(
+def test_validate_missing_ego_track_included():
+    scene = (
         SceneBuilder.empty()
         .add_sensor("rgb_center")
         .add_frame()
         .add_bbox(sensor_id="rgb_center")
-        .result.to_json()
-        .model_dump_json()
+        .result
     )
+    scene_dict = scene_to_dict(scene)
+
+    assert len(validate(scene_dict, validate_for_missing_ego_track=False)) == 0
+    assert len(validate(scene_dict, validate_for_missing_ego_track=True)) == 1
+
+
+def test_validate_sensors_included():
+    scene = SceneBuilder.empty().add_sensor("rgb_unknown").result
+    scene_dict = scene_to_dict(scene)
+
+    assert len(validate(scene_dict, validate_for_sensors=False)) == 0
+    assert len(validate(scene_dict, validate_for_sensors=True)) == 1
 
     actual = validate(scene_dict)
     assert len(actual) == 1
 
 
-def test_wrong_sensor_name_issue():
-    scene_dict = json.loads(
-        SceneBuilder.empty().add_sensor("rgb_unknown").result.to_json().model_dump_json()
-    )
-
-    actual = validate(scene_dict)
-    assert len(actual) == 1
-
-
-def test_validate_uris():
+def test_validate_uris_included():
     scene = SceneBuilder.empty().add_sensor("lidar").add_frame(1).add_poly3d().result
     scene.frames[1].sensors["lidar"] = SensorReference(timestamp=Decimal(0), uri="/INVALID/0.pcd")
+    scene_dict = scene_to_dict(scene)
 
-    assert len(validate(scene_to_dict(scene), validate_for_uris=False)) == 0
-    assert len(validate(scene_to_dict(scene), validate_for_uris=True)) == 1
+    assert len(validate(scene_dict, validate_for_uris=False)) == 0
+    assert len(validate(scene_dict, validate_for_uris=True)) == 1
 
 
 if __name__ == "__main__":
